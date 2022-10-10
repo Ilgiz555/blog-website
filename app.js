1acf4503aa8ad08302a2abbb3ae930d863805b9a
ILGIZ <ilgiz.zhaparov.on@gmail.com>
2022-10-10 20:21:41 +0800

    challenge 11 done

--- app.js
+++ app.js
@@ -16,8 +16,12 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
+let posts = [];
+
+
 app.get("/", function(req, res) {
   res.render("home", {startingContent: homeStartingContent});
+  console.log(posts);
 });
 
 app.get("/about", function(req, res) {
@@ -37,6 +41,11 @@ app.post("/compose", function(req, res) {
     title: req.body.postTitle,
     content: req.body.postBody
   };
+
+  posts.push(post);
+
+  res.redirect("/");
+
 });
 
 
